5a4fccdaed49df01cf54c3014c7c574a6abae6aa
keyzeroc <blinkdagger@bigmir.net>
2019-03-01 10:42:19 +0200

    Update score-storage.js

--- js/score-storage.js
+++ js/score-storage.js
@@ -1,6 +1,18 @@
+/**
+ * author: keyzeroc
+ * 
+ * functions to work with highscores array in localStorage
+ */
 var scoreStorageName="highscores";
 /**
-scoreObj constructor **/
+ * represents score
+ * @constructor
+ * @param {int} words - total words user typed.
+ * @param {double} kps - user's keys per second.
+ * @param {double} wps - users words per second.
+ * @param {int} tickDelay - user's tick delay.
+ * @param {int} wordDelay - user's word spawn delay.
+ */
 function scoreObj(words,kps,wps,tickDelay,wordDelay){
 	this.words=words;
 	this.kps=kps;
@@ -9,20 +21,28 @@ function scoreObj(words,kps,wps,tickDelay,wordDelay){
 	this.wordDelay=wordDelay;
 }
 /**
-create score object by given attributes **/
+ * constructs new scoreObj object from given params and returns it.
+ * @param {int} words - total words user typed.
+ * @param {double} kps - user's keys per second.
+ * @param {double} wps - users words per second.
+ * @param {int} tickDelay - user's tick delay.
+ * @param {int} wordDelay - user's word spawn delay.
+ */
 function createScoreObject(words,kps,wps,tickDelay,wordDelay) {
 	return new scoreObj(words,kps,wps,tickDelay,wordDelay);
 }
 /**
-compare function (decr order) **/
+ * compare function that's used to sort in decreasing order.
+ * @param {int} a - first number.
+ * @param {int} b - second number.
+ */
 function compareScores(a, b) {
   return b.words - a.words;
 }
 /**
-add new score to all scores (score top5 scores only)
-if score is not better then any of 5 top scores then it's not added
-if score is better then add it to array, sort array, and then remove 6th score from array, so only 5 top remains
-**/
+ * adds new score to all scores if score is good enough
+ * @param {scoreObj} newScoreObj - score object to store
+ */
 function addNewScore(newScoreObj){
 	let scoresArray = getScoreArray();
 	if(scoresArray.length<5){
@@ -41,7 +61,9 @@ function addNewScore(newScoreObj){
 	}
 }
 /**
-get scores array from localStorage **/
+ * gets highscores array from localStorage and returns it
+ * if it's not present - creates empty array and returns it
+ */
 function getScoreArray() {
 	let itemsArray = localStorage.getItem(scoreStorageName);
 	if (itemsArray == null || itemsArray =="") {
@@ -53,12 +75,15 @@ function getScoreArray() {
 	return itemsArray;
 }
 /**
-set scores array in localStorage **/
+ * sets highscores array to localStorage
+ * @param {Array} newArray 
+ */
 function setScoreArray(newArray){
 	localStorage.setItem(scoreStorageName, JSON.stringify(newArray));
 }
 /**
-clear scores array from localStorage **/
+ * clears all highscores in localStorage
+ */
 function clearAllScores(){
 	localStorage.removeItem(scoreStorageName);
 }
